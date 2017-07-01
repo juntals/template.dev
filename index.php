eb11d6df8bd33ad8ce29548d4eff9a8a9bf95dcc
jun <norberto.libago@wingedmedia.com>
2017-07-01 22:55:05 +0900

    Layout fix for bowral

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     
     
     <!-- font embed -->
-    <link href="https://fonts.googleapis.com/css?family=Lato|Lora" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i,900,900i|Lora:400,400i,700,700i" rel="stylesheet">
     
     <!-- default theme -->
     <link href="stylesheets/css/base/default.css" rel="stylesheet">
@@ -61,34 +61,34 @@
                             <a class="circle-social" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
                         </li>
                         <li>
-                            <a class="square-social" href="#"><i class="fa fa-user-plus" aria-hidden="true"></i></a>
+                            <a class="square-social" href="#">Login</a>
                         </li>
                         <li>
-                            <a class="square-social" href="#"><i class="fa fa-user-plus" aria-hidden="true"></i></a>
+                            <a class="square-social" href="#">Join</a>
                         </li>
                     </ul>
     
                     <ul class="nav navbar-nav pull-right">
                         <li>
-                            <a href="#">Home</a>
+                            <a class="hvr-bounce-to-top" href="#">Home</a>
                         </li>
                         <li>
-                            <a href="#">Business</a>
+                            <a class="hvr-bounce-to-top" href="#">Business</a>
                         </li>
                         <li>
-                            <a href="#">Categories</a>
+                            <a class="hvr-bounce-to-top" href="#">Categories</a>
                         </li>
                         <li>
-                            <a href="#">Store Locator</a>
+                            <a class="hvr-bounce-to-top" href="#">Store Locator</a>
                         </li>
                         <li>
-                            <a href="#">Blog</a>
+                            <a class="hvr-bounce-to-top" href="#">Blog</a>
                         </li>
                         <li>
-                            <a href="#">About</a>
+                            <a class="hvr-bounce-to-top" href="#">About</a>
                         </li>
                         <li>
-                            <a href="#">Contact</a>
+                            <a class="hvr-bounce-to-top" href="#">Contact</a>
                         </li>
                     </ul>
 
@@ -108,13 +108,13 @@
         <div class="vertical-center-child">
 
             <div class="container">
-                <h2 class="title font-special"><span class="font-special count">552014</span> Customer Reviews for <br>businesses in Australia </h2>  
+                <h2 class="title font-special align-center"><span class="font-special count">552014</span> Customer Reviews for <br>businesses in Australia </h2>  
 
                 <div class="main-search">
                     <form action="">
                         <input type="text" placeholder="What are you looking for?">  
                         <input type="text" placeholder="Where? (suburb, postcode, or city)">  
-                        <button class="search-submit" ><i class="fa fa-search" aria-hidden="true"></i> Find</button>
+                        <button class="search-submit hvr-grow" ><i class="fa fa-search" aria-hidden="true"></i> Find</button>
                     </form>
                 </div>
             </div>
@@ -139,29 +139,77 @@
         </div>
     </section>
 
-    <section>
+    <section class="gradient-bg">
         <div class="container">
 
-            <h2 class="title">Heading text <span>Special</span></h2>
+            <div class="row">
 
-            <a href="">test</a> | <a href="">test</a> | <a href="">test</a> | <a href="">test</a>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, expedita quis inventore soluta nisi quam, aut ducimus tempora ipsam, quisquam iste assumenda. Explicabo maxime ad ab sed. Dolore, necessitatibus hic provident iure eligendi, veritatis maxime quo omnis harum obcaecati cum porro aperiam consectetur consequuntur temporibus praesentium. Delectus veniam earum quod optio quis quae nobis libero nisi, maxime quasi magni eos eum non voluptas quam vel autem ab unde soluta quia cupiditate, at veritatis assumenda. Cum culpa voluptatibus corrupti cupiditate tenetur reprehenderit cumque, nulla nisi fuga laudantium blanditiis quae dolores, officia architecto, eaque voluptas maxime doloribus commodi in! Omnis assumenda laborum ut recusandae earum maiores eum quidem illum cupiditate voluptates, aliquid veritatis expedita, tempora unde nemo impedit voluptas id, nihil accusamus! Beatae iusto facilis sint sit quia eaque repudiandae harum! Autem quasi commodi praesentium possimus culpa necessitatibus cumque quo voluptatem deserunt consequuntur vel ullam, odio tenetur nesciunt obcaecati iusto facere sapiente animi, mollitia atque accusamus officiis! Minima nostrum officia aliquam nam nihil esse iste reprehenderit voluptate molestiae ea mollitia, labore incidunt blanditiis sunt rerum. Itaque, labore neque sit modi aut quibusdam, cumque, iusto pariatur unde aspernatur id! Dolores perspiciatis, veritatis similique asperiores amet ipsum, porro veniam voluptas ab accusantium magni laborum ipsam quos non modi, perferendis ratione, voluptate! Dolores illo sapiente accusantium placeat ut quas, ratione quos expedita voluptas impedit cum, repellendus aliquid, fugiat omnis eum sequi. Ipsam, molestias magnam! Consectetur minima deserunt consequuntur perferendis quisquam, illo minus porro maxime magnam expedita maiores tempora, rem, nihil atque veritatis nulla facere ipsa error harum voluptates ipsum doloribus numquam aperiam iste? Culpa, numquam, iusto quibusdam magnam distinctio labore error quisquam minima quam nisi ad! Magnam dolorem quasi expedita quos, impedit corrupti id, nisi consectetur rem iusto rerum ab soluta, delectus sed? Autem repellendus ducimus, corrupti, quia quisquam omnis velit qui, aut eveniet obcaecati magnam perferendis. Quibusdam asperiores sapiente impedit tempora, suscipit a animi. Itaque, labore? Totam sint, nostrum non sequi porro, repellat voluptas laboriosam harum reprehenderit laborum quisquam, fugiat magni! Placeat, quod, ex! Recusandae labore inventore quisquam quod autem odit ad iure accusantium nemo aliquid, fuga sint, architecto nam assumenda eius minus praesentium cumque. Nihil adipisci dolore sed aperiam, voluptas velit laboriosam unde. Magnam ea cupiditate harum temporibus est, ad recusandae, animi nam esse reiciendis, sint quasi praesentium et totam odio tempora quisquam quos. Dicta numquam dolore assumenda alias, odit reprehenderit sequi dolores deserunt, velit ab, nisi laudantium necessitatibus, repellendus aperiam. Tempora tempore similique mollitia facere eaque quia, quam corporis natus. Numquam distinctio ex commodi adipisci, fuga quia error veniam corrupti sequi, praesentium aliquam officia esse, eius assumenda blanditiis natus. Corporis dolore autem possimus, blanditiis quisquam distinctio consequuntur facilis consectetur eaque, unde consequatur praesentium reprehenderit aspernatur, temporibus nesciunt. Omnis maiores esse cumque, voluptates quod repellat accusantium, id aut dicta in doloribus dolorem, consequatur natus, repudiandae. Consectetur accusantium repudiandae qui velit similique soluta veritatis maiores quasi, aperiam voluptatibus quia, autem illo dolorem quod veniam dolorum aliquid iure omnis molestias facere sint! Officiis praesentium laborum, quibusdam unde alias nesciunt. Quisquam tempora aspernatur, expedita laborum repellendus?</p>
+                <div class="col-md-6">
+                    <h2 class="title font-special align-left padding-bottom-40">We are Bowral <span>Australia</span></h2>
+
+                    <p>HAIR.COM.AU is an online hair directory in Australia that provides a socially influenced recommendation platform that gives contact information, social ranking and other information related to local hair salons together with hair related services within the country.</p>
+
+                    <div class="our-method">
+                        <a class="hvr-fade" href=""><i class="fa fa-hashtag" aria-hidden="true"></i>SOCIALLY REWARDING</a>
+                        <a class="hvr-fade" href=""><i class="fa fa-bicycle" aria-hidden="true"></i>NEAR ME</a>
+                        <a class="hvr-fade" href=""><i class="fa fa-bullseye" aria-hidden="true"></i>EXPERIENCE</a>
+                    </div>
+                    
+                </div>
+                
+                <div class="col-md-6">
+                    <div class="text-center">
+                        <img class="img-responsive" src="http://lorempixel.com/700/500/" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
     <section>
         <div class="container">
 
-            <h2 class="title font-special">Heading text <span>Special</span></h2>
+            <h2 class="title font-special align-center">Our <span>Services</span></h2>
 
-            <p>Lorem ipsum dolord sit amet, consectetur adipisicing elit. Nemo, expedita quis inventore soluta nisi quam, aut ducimus tempora ipsam, quisquam iste assumenda. Explicabo maxime ad ab sed. Dolore, necessitatibus hic provident iure eligendi, veritatis maxime quo omnis harum obcaecati cum porro aperiam consectetur consequuntur temporibus praesentium. Delectus veniam earum quod optio quis quae nobis libero nisi, maxime quasi magni eos eum non voluptas quam vel autem ab unde soluta quia cupiditate, at veritatis assumenda. Cum culpa voluptatibus corrupti cupiditate tenetur reprehenderit cumque, nulla nisi fuga laudantium blanditiis quae dolores, officia architecto, eaque voluptas maxime doloribus commodi in! Omnis assumenda laborum ut recusandae earum maiores eum quidem illum cupiditate voluptates, aliquid veritatis expedita, tempora unde nemo impedit voluptas id, nihil accusamus! Beatae iusto facilis sint sit quia eaque repudiandae harum! Autem quasi commodi praesentium possimus culpa necessitatibus cumque quo voluptatem deserunt consequuntur vel ullam, odio tenetur nesciunt obcaecati iusto facere sapiente animi, mollitia atque accusamus officiis! Minima nostrum officia aliquam nam nihil esse iste reprehenderit voluptate molestiae ea mollitia, labore incidunt blanditiis sunt rerum. Itaque, labore neque sit modi aut quibusdam, cumque, iusto pariatur unde aspernatur id! Dolores perspiciatis, veritatis similique asperiores amet ipsum, porro veniam voluptas ab accusantium magni laborum ipsam quos non modi, perferendis ratione, voluptate! Dolores illo sapiente accusantium placeat ut quas, ratione quos expedita voluptas impedit cum, repellendus aliquid, fugiat omnis eum sequi. Ipsam, molestias magnam! Consectetur minima deserunt consequuntur perferendis quisquam, illo minus porro maxime magnam expedita maiores tempora, rem, nihil atque veritatis nulla facere ipsa error harum voluptates ipsum doloribus numquam aperiam iste? Culpa, numquam, iusto quibusdam magnam distinctio labore error quisquam minima quam nisi ad! Magnam dolorem quasi expedita quos, impedit corrupti id, nisi consectetur rem iusto rerum ab soluta, delectus sed? Autem repellendus ducimus, corrupti, quia quisquam omnis velit qui, aut eveniet obcaecati magnam perferendis. Quibusdam asperiores sapiente impedit tempora, suscipit a animi. Itaque, labore? Totam sint, nostrum non sequi porro, repellat voluptas laboriosam harum reprehenderit laborum quisquam, fugiat magni! Placeat, quod, ex! Recusandae labore inventore quisquam quod autem odit ad iure accusantium nemo aliquid, fuga sint, architecto nam assumenda eius minus praesentium cumque. Nihil adipisci dolore sed aperiam, voluptas velit laboriosam unde. Magnam ea cupiditate harum temporibus est, ad recusandae, animi nam esse reiciendis, sint quasi praesentium et totam odio tempora quisquam quos. Dicta numquam dolore assumenda alias, odit reprehenderit sequi dolores deserunt, velit ab, nisi laudantium necessitatibus, repellendus aperiam. Tempora tempore similique mollitia facere eaque quia, quam corporis natus. Numquam distinctio ex commodi adipisci, fuga quia error veniam corrupti sequi, praesentium aliquam officia esse, eius assumenda blanditiis natus. Corporis dolore autem possimus, blanditiis quisquam distinctio consequuntur facilis consectetur eaque, unde consequatur praesentium reprehenderit aspernatur, temporibus nesciunt. Omnis maiores esse cumque, voluptates quod repellat accusantium, id aut dicta in doloribus dolorem, consequatur natus, repudiandae. Consectetur accusantium repudiandae qui velit similique soluta veritatis maiores quasi, aperiam voluptatibus quia, autem illo dolorem quod veniam dolorum aliquid iure omnis molestias facere sint! Officiis praesentium laborum, quibusdam unde alias nesciunt. Quisquam tempora aspernatur, expedita laborum repellendus?</p>
+            <ul class="center-card-block">
+                <li>
+                    <img src="https://hair.com.au/images/default-images/events-home.png" alt="">
+                    <h3>Events</h3>
+                        Presenting you Australia’s latest hair social events, gatherings, tradeshows and stylist conventions.
+                </li>
+                <li>
+                    <img src="https://hair.com.au/images/default-images/business-deal-home.png" alt="">
+                    <h3>Deals</h3>
+                        Let our user community know about special deals you have on offer!
+                </li>
+                <li>
+                    <img src="https://hair.com.au/images/default-images/ranking-home.png" alt="">
+                    <h3>Ranking </h3>
+                        Using your social media connections, you can see which hair businesses are ranking well!
+                </li>
+                <li>
+                    <img src="https://hair.com.au/images/default-images/nearby-home.png" alt="">
+                    <h3>Nearby</h3>
+                        When on your mobile you can quickly find the best hair services nearby!
+                </li>
+                <li>
+                    <img src="https://hair.com.au/images/default-images/promotion-home.png" alt="">
+                    <h3>Promotion</h3>
+                        We provide monthly promotions for our premium listed hair businesses.
+                </li>
+                <li>
+                    <img src="https://hair.com.au/images/default-images/engagement-home.png" alt="">
+                    <h3>Connect</h3>
+                        Connect with us via your FB profile to rank your favourite hair services.
+                </li>
+            </ul>
         </div>
     </section>
 
     <section>
         <div class="container">
 
-            <h2 class="title">Heading text <span>Special</span></h2>
+            <h2 class="title">Our  <span>Network</span></h2>
 
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, expedita quis inventore soluta nisi quam, aut ducimus tempora ipsam, quisquam iste assumenda. Explicabo maxime ad ab sed. Dolore, necessitatibus hic provident iure eligendi, veritatis maxime quo omnis harum obcaecati cum porro aperiam consectetur consequuntur temporibus praesentium. Delectus veniam earum quod optio quis quae nobis libero nisi, maxime quasi magni eos eum non voluptas quam vel autem ab unde soluta quia cupiditate, at veritatis assumenda. Cum culpa voluptatibus corrupti cupiditate tenetur reprehenderit cumque, nulla nisi fuga laudantium blanditiis quae dolores, officia architecto, eaque voluptas maxime doloribus commodi in! Omnis assumenda laborum ut recusandae earum maiores eum quidem illum cupiditate voluptates, aliquid veritatis expedita, tempora unde nemo impedit voluptas id, nihil accusamus! Beatae iusto facilis sint sit quia eaque repudiandae harum! Autem quasi commodi praesentium possimus culpa necessitatibus cumque quo voluptatem deserunt consequuntur vel ullam, odio tenetur nesciunt obcaecati iusto facere sapiente animi, mollitia atque accusamus officiis! Minima nostrum officia aliquam nam nihil esse iste reprehenderit voluptate molestiae ea mollitia, labore incidunt blanditiis sunt rerum. Itaque, labore neque sit modi aut quibusdam, cumque, iusto pariatur unde aspernatur id! Dolores perspiciatis, veritatis similique asperiores amet ipsum, porro veniam voluptas ab accusantium magni laborum ipsam quos non modi, perferendis ratione, voluptate! Dolores illo sapiente accusantium placeat ut quas, ratione quos expedita voluptas impedit cum, repellendus aliquid, fugiat omnis eum sequi. Ipsam, molestias magnam! Consectetur minima deserunt consequuntur perferendis quisquam, illo minus porro maxime magnam expedita maiores tempora, rem, nihil atque veritatis nulla facere ipsa error harum voluptates ipsum doloribus numquam aperiam iste? Culpa, numquam, iusto quibusdam magnam distinctio labore error quisquam minima quam nisi ad! Magnam dolorem quasi expedita quos, impedit corrupti id, nisi consectetur rem iusto rerum ab soluta, delectus sed? Autem repellendus ducimus, corrupti, quia quisquam omnis velit qui, aut eveniet obcaecati magnam perferendis. Quibusdam asperiores sapiente impedit tempora, suscipit a animi. Itaque, labore? Totam sint, nostrum non sequi porro, repellat voluptas laboriosam harum reprehenderit laborum quisquam, fugiat magni! Placeat, quod, ex! Recusandae labore inventore quisquam quod autem odit ad iure accusantium nemo aliquid, fuga sint, architecto nam assumenda eius minus praesentium cumque. Nihil adipisci dolore sed aperiam, voluptas velit laboriosam unde. Magnam ea cupiditate harum temporibus est, ad recusandae, animi nam esse reiciendis, sint quasi praesentium et totam odio tempora quisquam quos. Dicta numquam dolore assumenda alias, odit reprehenderit sequi dolores deserunt, velit ab, nisi laudantium necessitatibus, repellendus aperiam. Tempora tempore similique mollitia facere eaque quia, quam corporis natus. Numquam distinctio ex commodi adipisci, fuga quia error veniam corrupti sequi, praesentium aliquam officia esse, eius assumenda blanditiis natus. Corporis dolore autem possimus, blanditiis quisquam distinctio consequuntur facilis consectetur eaque, unde consequatur praesentium reprehenderit aspernatur, temporibus nesciunt. Omnis maiores esse cumque, voluptates quod repellat accusantium, id aut dicta in doloribus dolorem, consequatur natus, repudiandae. Consectetur accusantium repudiandae qui velit similique soluta veritatis maiores quasi, aperiam voluptatibus quia, autem illo dolorem quod veniam dolorum aliquid iure omnis molestias facere sint! Officiis praesentium laborum, quibusdam unde alias nesciunt. Quisquam tempora aspernatur, expedita laborum repellendus?</p>
         </div>
