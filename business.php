073602e98d1d2ed58053c4697a1fb67a430f8402
juntals <norberto.libago@wingedmedia.com>
2017-07-03 14:54:17 +0800

    Adding business page

---
+++ business.php
@@ -0,0 +1,399 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Bowral</title>
+
+    <link rel="icon" href="https://www.bowral.com.au/public_html/assets/website/img/favicon/favicon.png">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="stylesheets/vendors/bootstrap.min.css" rel="stylesheet">
+    <link href="stylesheets/vendors/hover/css/hover-min.css" rel="stylesheet">
+    <link href="stylesheets/vendors/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    
+    
+    <!-- font embed -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i,900,900i|Lora:400,400i,700,700i" rel="stylesheet">
+    
+    <!-- default theme -->
+    <link href="stylesheets/css/base/default.css" rel="stylesheet">
+    <link href="stylesheets/css/base/color.css" rel="stylesheet">
+    <link href="stylesheets/css/base/font.css" rel="stylesheet">
+
+    <link href="stylesheets/css/main.css" rel="stylesheet">
+    
+ 
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="common-page">
+
+    <!-- Navigation -->
+    <header id="header-2">
+        <nav class="navbar navbar-inverse " data-spy="affix" data-offset-top="150" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" id="logo" class="navbar-brand"><img class="" src="/images/logo.png" alt=""></a></h1>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                    <ul class="nav navbar-nav pull-right quick-menu">
+                        <li>
+                            <a class="circle-social" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a class="square-social" href="#">Login</a>
+                        </li>
+                        <li>
+                            <a class="square-social" href="#">Join</a>
+                        </li>
+                    </ul>
+    
+                    <ul class="nav navbar-nav pull-right">
+                        <li>
+                            <a class="hvr-bounce-to-top" href="#">Home</a>
+                        </li>
+                        <li>
+                            <a class="hvr-bounce-to-top" href="#">Business</a>
+                        </li>
+                        <li>
+                            <a class="hvr-bounce-to-top" href="#">Categories</a>
+                        </li>
+                        <li>
+                            <a class="hvr-bounce-to-top" href="#">Store Locator</a>
+                        </li>
+                        <li>
+                            <a class="hvr-bounce-to-top" href="#">Blog</a>
+                        </li>
+                        <li>
+                            <a class="hvr-bounce-to-top" href="#">About</a>
+                        </li>
+                        <li>
+                            <a class="hvr-bounce-to-top" href="#">Contact</a>
+                        </li>
+                    </ul>
+
+                    
+
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+    </header>
+
+    <!-- Page Content -->
+
+    <section class="fluid-wrapper">
+    	<div id="cmap" class="contactmap" style="height: 70vh;"></div>
+    </section>
+
+    <section class="float-top">
+    	<div class="container">
+    		<div class="section-search">
+	            <form action="">
+	                <input type="text" placeholder="What are you looking for?">  
+	                <input type="text" placeholder="Where? (suburb, postcode, or city)">  
+	                <button class="search-submit hvr-grow" ><i class="fa fa-search" aria-hidden="true"></i> Find</button>
+	            </form>
+	        </div>
+    	</div>
+    </section>
+
+    <section>
+    	<div class="container">
+    		
+    		<div class="row">
+    			
+    			<div class="col-md-9">
+    				<div class="pagination-counter">
+    					<p>Showing 1 - 5 of 591 Businesses</p>
+    				</div>
+    				<div class="business-listing">
+
+    					<div class="business-item">
+    					
+    						<div class="business-image">
+	    						<img src="http://lorempixel.com/400/300/sports/" alt="">
+	    					</div>
+
+	    					<div class="business-map">
+	    						<span>1</span>
+	    					</div>
+
+	    					<div class="business-details">
+	    						<h4>Skin Deep Permanent Hair Removal</h4>
+	    						<h4>hairdresser</h4>
+								<ul>
+									<li>3/230 Toorak Road West, Melbourne, South Yarra, VIC</li>
+									<li>1300 868 233</li>
+									<li>Email</li>
+									<li>Website</li>
+								</ul>
+	    						
+	    						<div class="business-review">
+	    							star
+	    						</div>
+	    					</div>
+
+    					</div>
+
+    					<div class="business-item">
+    					
+    						<div class="business-image">
+	    						<img src="http://lorempixel.com/400/300/sports/" alt="">
+	    					</div>
+
+	    					<div class="business-map">
+	    						<span>1</span>
+	    					</div>
+
+	    					<div class="business-details">
+	    						<h4>Skin Deep Permanent Hair Removal</h4>
+	    						<h4>hairdresser</h4>
+								<ul>
+									<li>3/230 Toorak Road West, Melbourne, South Yarra, VIC</li>
+									<li>1300 868 233</li>
+									<li>Email</li>
+									<li>Website</li>
+								</ul>
+	    						
+	    						<div class="business-review">
+	    							star
+	    						</div>
+	    					</div>
+
+    					</div>
+
+    					<div class="business-item">
+    					
+    						<div class="business-image">
+	    						<img src="http://lorempixel.com/400/300/sports/" alt="">
+	    					</div>
+
+	    					<div class="business-map">
+	    						<span>1</span>
+	    					</div>
+
+	    					<div class="business-details">
+	    						<h4>Skin Deep Permanent Hair Removal</h4>
+	    						<h4>hairdresser</h4>
+								<ul>
+									<li>3/230 Toorak Road West, Melbourne, South Yarra, VIC</li>
+									<li>1300 868 233</li>
+									<li>Email</li>
+									<li>Website</li>
+								</ul>
+	    						
+	    						<div class="business-review">
+	    							star
+	    						</div>
+	    					</div>
+
+    					</div>
+
+    				</div>
+
+    				<div class="text-center margin-bottom-40">
+					    <ul class="pagination">
+					        <li class="disabled"><span>«</span></li>
+					        <li class="active"><span>1</span></li>
+					        <li><a href="https://hair.com.au/search?location=South+Australia&amp;q=&amp;page=2">2</a></li>
+					        <li><a href="https://hair.com.au/search?location=South+Australia&amp;q=&amp;page=3">3</a></li>
+					        <li><a href="https://hair.com.au/search?location=South+Australia&amp;q=&amp;page=4">4</a></li>
+					        <li><a href="https://hair.com.au/search?location=South+Australia&amp;q=&amp;page=5">5</a></li>
+					        <li><a href="https://hair.com.au/search?location=South+Australia&amp;q=&amp;page=6">6</a></li>
+					        <li><a href="https://hair.com.au/search?location=South+Australia&amp;q=&amp;page=7">7</a></li>
+					        <li><a href="https://hair.com.au/search?location=South+Australia&amp;q=&amp;page=8">8</a></li>
+					        <li class="disabled"><span>...</span></li>
+					        <li><a href="https://hair.com.au/search?location=South+Australia&amp;q=&amp;page=105">105</a></li>
+					        <li><a href="https://hair.com.au/search?location=South+Australia&amp;q=&amp;page=106">106</a></li>
+					        <li><a href="https://hair.com.au/search?location=South+Australia&amp;q=&amp;page=2" rel="next">»</a></li>
+					    </ul>
+					    <!-- web-pagination -->
+					    <div>Page <span>1 of 106 </span></div>
+					</div>
+
+    			</div>
+
+    			<div class="col-md-3">
+    				<div class="upcoming-events">
+    					<div class="event-date">
+    						<span>9</span>
+    						<span>March</span>
+    					</div>
+    					<div class="event-image">
+    						<img src="http://lorempixel.com/300/200/sports/" alt="">
+    					</div>
+    					<div class="event-details">
+    						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est expedita error eaque laudantium, culpa laboriosam iusto nisi quia aperiam. Necessitatibus autem amet perferendis optio at!</p>
+    					</div>
+    				</div>
+    			</div>
+
+    		</div>
+
+    	</div>
+    </section>
+
+    <footer class="text-center">
+        <div class="footer-upper-bar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul class="footer-menu">
+                            <li><a class="hvr-shutter-out-vertical" href="https://hair.com.au/home">Home</a></li>
+                            <li><a class="hvr-shutter-out-vertical" href="https://hair.com.au/search/q">Business</a></li>
+                            <li>
+                                <a class="hvr-shutter-out-vertical" href="https://hair.com.au/categories">Categories</a>
+                            </li>
+                            <li>
+                                <a class="hvr-shutter-out-vertical" href="https://hair.com.au/store-locator">Store Locator</a>
+                            </li>
+                            <li>
+                                <a class="hvr-shutter-out-vertical" href="https://hair.com.au/blog">Blog</a>
+                            </li>
+                            <li>
+                                <a class="hvr-shutter-out-vertical" href="https://hair.com.au/about-us">About Us</a>
+                            </li>
+                            <li>
+                                <a class="hvr-shutter-out-vertical" href="https://hair.com.au/privacy">Privacy</a>
+                            </li>
+                            <li>
+                                <a class="hvr-shutter-out-vertical" href="https://hair.com.au/terms-of-service">Terms of Service</a>
+                            </li>
+                            <li>
+                                <a class="hvr-shutter-out-vertical" href="https://hair.com.au/faqs">FAQ's</a>
+                            </li>
+                            <li><a class="hvr-shutter-out-vertical" href="#feedbackModal" data-toggle="modal">Sites Feedback</a></li>
+                            
+                                
+                            
+                            <!-- Modal -->
+                            
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
+        </div><!-- ../footer-upper-bar -->
+
+        <div class="footer-bottom-bar">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="left-image-service">
+                            <div class="img-wrapper">
+                                <a href="https://hair.com.au/home"><img src="/images/logo.png"" alt="Hair.com.au - socially rewarding"></a>
+                            </div>
+                            <div class="text-wrapper">
+                                <p>We provide a socially driven recommendation platform that provides contact information, social ranking &amp; other related information about local hair salons and hair related services in Australia.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <ul>
+                            <li>PO BOX 620</li>
+                            <li>Terrigal NSW 2260</li>
+                            <li><a href="tel:(02) 8007 5521">Tel: (02) 8091 3859</a></li>
+                            <li><a href="mailto:info@hair.com.au">info@hair.com.au</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 text-left">
+                        <div class="text-wrapper">
+                            <p>Sign-up for our mailing list to get latest updates and offers.</p>
+                        </div>
+                        <div class="newsletter">
+                            <form id="newsletter" name="newsletter" role="form" action="https://hair.com.au/home" method="POST" accept-charset="utf-8" class="ng-pristine ng-valid">
+                                <input type="hidden" name="_token" value="Fss2MHbYm6rbErEo64iwQgPIVq3GmuXugM4U0ltn" autocomplete="off">
+                                <label for="email">Email Address:</label>
+                                <div class="input-group">
+                                    <input id="email" type="text" class="form-control" name="email" placeholder="Your Email Address">
+                                    <span class="input-group-btn">
+                                            <button id="btn_id" class="btn btn-secondary" type="submit">Sign-up</button>
+                                        </span>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                     <p class="margin-top-10">Visit and Like Us:</p>
+                        <ul class="social-icons footer-social-icons">
+                            <li><a href="https://www.facebook.com/haircomau/" target="_blank">
+                                <i class="fa fa-facebook-square" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                </div>
+
+                <div class="row">
+                    <p class="copyright">Copyright © 2017. All Rights Reserved | Developed by: <a href="http://rushmedia.ph/" target="_blank">RushMedia</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="/js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/jarallax/jarallax.js"></script>
+    <script src="/js/jarallax/jarallax-video.js"></script>
+    <script src="/js/jquery-countTo/jquery.countTo.js"></script>
+    <script src="/js/smoothScroll/scrollSpeed.js"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAt9TyI6VkaXWwv_ZWN-dtWjgRKk-L8WjQ&libraries=places"></script>cd
+    <script>
+        jQuery.scrollSpeed(100, 800);
+    </script>
+    <script>
+
+    $(document).ready(function(){
+      
+        var myLatLng = {lat: -33.44743, lng: 151.44527};
+
+        var map = new google.maps.Map(document.getElementById('cmap'), {
+            zoom: 16,
+            center: myLatLng,
+            scrollwheel: false
+        });
+
+        var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'My map'
+        });    
+
+        });
+
+  
+      </script>
+
+    <script src="/js/main.js"></script>
+
+</body>
+
+</html>
